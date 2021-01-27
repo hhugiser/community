a70eb14866178b5c877e97821dccd84a40f37f3b
hhugiser <15651780595@163.com>
2021-01-27 20:25:08 +0800

    ectract github config from code to properties

--- src/main/java/com/hhugiser/community/controller/AuthorizeController.java
+++ src/main/java/com/hhugiser/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.hhugiser.community.dto.AccessTokenDTO;
 import com.hhugiser.community.dto.GithubUser;
 import com.hhugiser.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,15 +21,24 @@ public class AuthorizeController {
   @Autowired
   private GithubProvider githubProvider;
 
+  @Value("${github.client.id}")
+  private String clientId;
+
+  @Value(("${github.client.secret}"))
+  private String clientSecret;
+
+  @Value("${github.redirect.uri}")
+  private String redirectUri;
+
   @GetMapping("/callback")
   public String callback(@RequestParam(name = "code") String code,
                          @RequestParam(name = "state") String state) {
     AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
     accessTokenDTO.setCode(code);
     accessTokenDTO.setState(state);
-    accessTokenDTO.setClient_id("7697a44b0a6680bd50b7");
-    accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
-    accessTokenDTO.setClient_secret("2b105fd1cd7540b61f17a8c1cf56ef9d5a4356e8");
+    accessTokenDTO.setClient_id(clientId);
+    accessTokenDTO.setRedirect_uri(redirectUri);
+    accessTokenDTO.setClient_secret(clientSecret);
     String accessToken = githubProvider.getAccessToken(accessTokenDTO);
     GithubUser user = githubProvider.getUser(accessToken);
     System.out.println(user.getName());
--- src/main/java/com/hhugiser/community/controller/IndexController.java
+++ src/main/java/com/hhugiser/community/controller/IndexController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-
   @GetMapping("/")
   public String index() {
     return "index";
